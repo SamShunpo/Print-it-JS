6a1668c9b81414567e713625bfb89acef2e33bfa
Samuel Gleize-Bourras <samuel@shunpo.fr>
2023-12-09 20:59:14 +0100

    Ajout Bullet point

--- assets/script.js
+++ assets/script.js
@@ -26,3 +26,21 @@ let rightArrow = document.querySelector(".arrow_right");
 rightArrow.addEventListener("click", (event) => {
 	console.log(event.target)
 });
+
+let bulletDivBlock = document.querySelector(".dots");
+let bulletId = 1
+
+for(let i = 1; i <= slides.length; i++) {
+	let bullet= document.createElement("span")
+	bullet.id= bulletId
+	if (bulletId === 1){
+		bullet.classList=("dot dot_selected");
+	} else {
+		bullet.classList=("dot")
+	}
+	bulletDivBlock.appendChild(bullet);
+	bulletId = bulletId+i
+};
+
+
+
