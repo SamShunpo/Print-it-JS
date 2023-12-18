b89febdf585bc60cb1d170b5981b98f37555959c
Samuel Gleize-Bourras <samuel@shunpo.fr>
2023-12-18 13:51:33 +0100

    Fonctionnement Bullet OK

--- assets/script.js
+++ assets/script.js
@@ -17,26 +17,52 @@ const slides = [
 	}
 ]
 
+let bulletDivBlock = document.querySelector(".dots");
+let bulletId = 0
+
+for(let i = 1; i <= slides.length; i++) {
+	let bullet = document.createElement("span")
+	bullet.id = bulletId
+	if(bulletId===0){
+		bullet.classList=("dot dot_selected")
+	}else{
+		bullet.classList=("dot")
+	}
+	bulletDivBlock.appendChild(bullet);
+	bulletId++
+};
+
+
 let leftArrow = document.querySelector(".arrow_left");
 let image = document.querySelector(".banner-img");
 let tagline = document.querySelector("#banner p");
 
 let imageIndex = 0
 
+
 leftArrow.addEventListener("click", () => {
+	let bulletSelected = document.getElementById(`${imageIndex}`)
+	bulletSelected.classList.remove("dot_selected")
 	if(imageIndex>0){
 		imageIndex--
 	}else {
 		imageIndex=(slides.length-1)
 	}
+	
 	image.src=`./assets/images/slideshow/${slides[imageIndex].image}`
 	let p = `<p>${slides[imageIndex].tagLine}</p>`
 	tagline.innerHTML = p
+
+	bulletSelected = document.getElementById(`${imageIndex}`)
+	bulletSelected.classList.add("dot_selected")
+
 });
  
 let rightArrow = document.querySelector(".arrow_right");
 
 rightArrow.addEventListener("click", () => {
+	let bulletSelected = document.getElementById(`${imageIndex}`)
+	bulletSelected.classList.remove("dot_selected")
 	if(imageIndex<(slides.length-1)){
 		imageIndex++
 	}else {
@@ -45,22 +71,11 @@ rightArrow.addEventListener("click", () => {
 	image.src=`./assets/images/slideshow/${slides[imageIndex].image}`
 	let p = `<p>${slides[imageIndex].tagLine}</p>`
 	tagline.innerHTML = p
+
+	bulletSelected = document.getElementById(`${imageIndex}`)
+	bulletSelected.classList.add("dot_selected")
+
 });
-
-let bulletDivBlock = document.querySelector(".dots");
-let bulletId = 0
-
-for(let i = 1; i <= slides.length; i++) {
-	let bullet= document.createElement("span")
-	bullet.id= bulletId
-	if (bulletId === 0){
-		bullet.classList=("dot dot_selected");
-	} else {
-		bullet.classList=("dot")
-	}
-	bulletDivBlock.appendChild(bullet);
-	bulletId++
-};
 
 
 
