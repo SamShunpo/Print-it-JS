a45a10f967164b9124af5bc1d8e1a6f7c2ebd5ee
Samuel Gleize-Bourras <samuel@shunpo.fr>
2024-01-30 11:43:38 +0100

    Init changeImageAndTitle at 0

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,95 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ];
 
 
 // Fonction pour changer l'image et le titre
 function changeImageAndTitle(index) {
-	imageIndex = parseInt(index);
-    const tagline = document.querySelector("#banner p");
-    const image = document.querySelector(".banner-img");
+	const banner = document.querySelector("#banner");
+	let tagline = document.querySelector("#banner p");
+	let image = document.querySelector(".banner-img");
+	if (!tagline) {
+		const p = document.createElement("p");
+		banner.prepend(p)
+		tagline = document.querySelector("#banner p");
+	}
+	if (!image) {
+		const img = document.createElement("img");
+		img.classList.add("banner-img");
+		banner.prepend(img)
+		image = document.querySelector(".banner-img");
+	}
 
-    image.src = `./assets/images/slideshow/${slides[index].image}`;
-    tagline.innerHTML = `<p>${slides[index].tagLine}</p>`;
+	image.src = `./assets/images/slideshow/${slides[index].image}`;
+	image.alt = slides[index].tagLine;
+	tagline.innerHTML = `<p>${slides[index].tagLine}</p>`;
 	updateSelectedBullet(index);
 }
 
 // Fonction pour mettre à jour la bullet point sélectionnée
 function updateSelectedBullet(newIndex) {
-    const bullets = document.querySelectorAll('.dot');
-    bullets.forEach(bullet => bullet.classList.remove('dot_selected'));
+	const bullets = document.querySelectorAll('.dot');
+	bullets.forEach(bullet => bullet.classList.remove('dot_selected'));
 
-    const bulletSelected = document.getElementById(newIndex);
-    bulletSelected.classList.add('dot_selected');
+	const bulletSelected = document.getElementById(newIndex);
+	bulletSelected.classList.add('dot_selected');
 }
 
 // Création des bullet points
 const bulletDivBlock = document.querySelector(".dots");
-let bulletId = 0;
 
 for (let i = 0; i < slides.length; i++) {
-    const bullet = document.createElement("span");
-    bullet.className = 'dot';
-    bullet.id = bulletId;
-    if (bulletId === 0) {
-        bullet.classList.add('dot_selected');
-    }
-    bullet.addEventListener("click", () => {
-        changeImageAndTitle(bullet.id);
+	const bullet = document.createElement("span");
+	bullet.className = 'dot';
+	bullet.id = i;
+	if (i === 0) {
+		bullet.classList.add('dot_selected');
+	}
+	bulletDivBlock.appendChild(bullet);
 
-    });
-    bulletDivBlock.appendChild(bullet);
-    bulletId++;
+	bullet.addEventListener("click", () => {
+		changeImageAndTitle(i);
+	});
 }
 
 // Gestion des flèches de navigation
-let leftArrow = document.querySelector(".arrow_left");
 let imageIndex = 0;
 
-leftArrow.addEventListener("click", () => {
+changeImageAndTitle(imageIndex);
 
-	if(imageIndex>0){
+const leftArrow = document.querySelector(".arrow_left");
+const rightArrow = document.querySelector(".arrow_right");
+
+leftArrow.addEventListener("click", () => {
+	if (imageIndex > 0) {
 		imageIndex--
-	}else {
-		imageIndex=(slides.length-1)
+	} else {
+		imageIndex = slides.length - 1
 	};
 	changeImageAndTitle(imageIndex);
-
-
 });
- 
-let rightArrow = document.querySelector(".arrow_right");
 
 rightArrow.addEventListener("click", () => {
-
-	if(imageIndex<(slides.length-1)){
+	if (imageIndex < slides.length - 1) {
 		imageIndex++
-	}else {
-		imageIndex=0
+	} else {
+		imageIndex = 0
 	};
 	changeImageAndTitle(imageIndex);
-
-
 });
 
